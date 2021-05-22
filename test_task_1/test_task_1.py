1d6ac96ffc16af5c90226ea7e4696b75b11f06d2
andrew030987 <andrew.sev87@gmail.com>
2021-05-23 00:12:53 +0300

    first commit

---
+++ test_task_1/test_task_1.py
@@ -0,0 +1,42 @@
+import requests
+import unittest
+
+class TestCrudMethods(unittest.TestCase):
+    def test_create(self):
+        c = requests.post('https://petstore.swagger.io/v2/user', json={
+        "id": 0,
+        "username": "UName",
+        "firstName": "Fname",
+        "lastName": "Lname",
+        "email": "test@test.com",
+        "password": "pass",
+        "phone": "123",
+        "userStatus": 0})
+        self.assertEqual(c.status_code, 200)
+        # self.assertTrue(c.ok)
+
+    def test_read(self):
+        r = requests.get('https://petstore.swagger.io/v2/user/UName')
+        self.assertEqual(r.status_code, 200)
+
+
+    def test_put(self):
+        u = requests.put('https://petstore.swagger.io/v2/user/Uname', json = 
+        {"id": 0,
+        "username": "UName",
+        "firstName": "Fname",
+        "lastName": "Lname",
+        "email": "a@a.com",
+        "password": "pass",
+        "phone": "123",
+        "userStatus": 0})
+        self.assertEqual(u.status_code, 200)
+
+
+    def test_delete(self):
+        d = requests.delete('https://petstore.swagger.io/v2/user/UName')
+        self.assertEqual(d.status_code, 200)
+    
+    
+
+
---
+++ test_task_2/test_task_2.py
@@ -0,0 +1,22 @@
+
+def first_rep(arr):
+    s = set()
+    for i in arr:
+        if i in s:
+            return i
+        else:
+            s.add(i)
+
+
+lst1 = ['a', 'b', 'c', 'c', 'a']
+lst2 = ['e', 'a', 'b', 'b', 'c', 'c', 'd', 'a', 'b']
+lst3 = ['a', 'c', 'a', 'c', 'e', 'a']
+
+assert first_rep(lst1) == 'c'
+assert first_rep(lst2) == 'b'
+assert first_rep(lst3) == 'a'
+
+print(first_rep(lst1))
+print(first_rep(lst2))
+print(first_rep(lst3))
+
