0ca625df2f342991c20ef4aff6605e82e85c3d9a
andrew030987 <andrew.sev87@gmail.com>
2021-05-24 15:34:33 +0300

    4th commit / Added some negative tests

--- test_task_1/test_task_1.py
+++ test_task_1/test_task_1.py
@@ -2,6 +2,9 @@ import requests
 import unittest
 
 class TestCrudOperations(unittest.TestCase):
+
+#Positive tests
+
     def test_create(self):
         c = requests.post('https://petstore.swagger.io/v2/user', json={
         "id": 0,
@@ -20,7 +23,7 @@ class TestCrudOperations(unittest.TestCase):
         self.assertEqual(r.status_code, 200)
 
 
-    def test_put(self):
+    def test_update(self):
         u = requests.put('https://petstore.swagger.io/v2/user/Uname', json = 
         {"id": 0,
         "username": "UName",
@@ -37,7 +40,47 @@ class TestCrudOperations(unittest.TestCase):
         d = requests.delete('https://petstore.swagger.io/v2/user/UName')
         self.assertTrue(d.ok)
     
+
+
+#Negative tests
     
+
+    def test_create_user_invalid_data(self):
+        c = requests.post('https://petstore.swagger.io/v2/user', data={
+        "id": 0,
+        "username": "Uname",
+        "firstName": "Fname",
+        "lastName": "Lname",
+        "email": "test@test.com",
+        "password": "pass",
+        "phone": "123",
+        "userStatus": 0})
+        self.assertFalse(c.ok)
+
+
+    def test_read_invalid_user(self):
+        r = requests.get('https://petstore.swagger.io/v2/user/SomeInvalidUserName')
+        self.assertEqual(r.status_code, 404)
+
+
+    def test_update_user_invalid_data(self):
+        u = requests.put('https://petstore.swagger.io/v2/user/Uname', data = 
+        {"id": 0,
+        "username": "UName",
+        "firstName": "Fname",
+        "lastName": "Lname",
+        "email": "a@a.com",
+        "password": "pass",
+        "phone": "123",
+        "userStatus": 0})
+        self.assertFalse(u.ok)
+
+
+    def test_delete_invalid_user(self):
+        d = requests.delete('https://petstore.swagger.io/v2/user/SomeInvalidUserName')
+        self.assertFalse(d.ok)
+
+
 
     
 if __name__ == '__main__':
